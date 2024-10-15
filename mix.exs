e0a578ac428fb403fc8ad8cff16f03985cd60d1a
Minsub Kim <rapidfsub@gmail.com>
2024-10-15 13:24:40 +0900

    mix deps.get

--- mix.exs
+++ mix.exs
@@ -33,19 +33,19 @@ defmodule Sonet.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:igniter, "~> 0.3"},
-      {:phoenix, "~> 1.7.14"},
-      {:phoenix_ecto, "~> 4.5"},
-      {:ecto_sql, "~> 3.10"},
+      {:igniter, ">= 0.0.0"},
+      {:phoenix, ">= 0.0.0"},
+      {:phoenix_ecto, ">= 0.0.0"},
+      {:ecto_sql, ">= 0.0.0"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 4.1"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_html, ">= 0.0.0"},
+      {:phoenix_live_reload, ">= 0.0.0", only: :dev},
       # TODO bump on release to {:phoenix_live_view, "~> 1.0.0"},
-      {:phoenix_live_view, "~> 1.0.0-rc.1", override: true},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.3"},
-      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
+      {:phoenix_live_view, ">= 0.0.0", override: true},
+      {:floki, ">= 0.0.0", only: :test},
+      {:phoenix_live_dashboard, ">= 0.0.0"},
+      {:esbuild, ">= 0.0.0", runtime: Mix.env() == :dev},
+      {:tailwind, ">= 0.0.0", runtime: Mix.env() == :dev},
       {:heroicons,
        github: "tailwindlabs/heroicons",
        tag: "v2.1.1",
@@ -53,14 +53,14 @@ defmodule Sonet.MixProject do
        app: false,
        compile: false,
        depth: 1},
-      {:swoosh, "~> 1.5"},
-      {:finch, "~> 0.13"},
-      {:telemetry_metrics, "~> 1.0"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.20"},
-      {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:swoosh, ">= 0.0.0"},
+      {:finch, ">= 0.0.0"},
+      {:telemetry_metrics, ">= 0.0.0"},
+      {:telemetry_poller, ">= 0.0.0"},
+      {:gettext, ">= 0.0.0"},
+      {:jason, ">= 0.0.0"},
+      {:dns_cluster, ">= 0.0.0"},
+      {:bandit, ">= 0.0.0"}
     ]
   end
 
