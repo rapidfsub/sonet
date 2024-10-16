dc87540af53353d3f8c5704fbebbebc5658d5bae
Minsub Kim <rapidfsub@gmail.com>
2024-10-16 18:24:07 +0900

    test order of policies in resource and fragment

--- test/sonet_lib/ash/policies_test.exs
+++ test/sonet_lib/ash/policies_test.exs
@@ -2,7 +2,7 @@ defmodule SonetLib.Ash.PoliciesTest do
   use Sonet.DataCase
 
   test "policy conditions are combined with and" do
-    defmodule Article do
+    defmodule Article1 do
       use Ash.Resource,
         domain: SonetLib.Domain,
         authorizers: [Ash.Policy.Authorizer]
@@ -24,8 +24,50 @@ defmodule SonetLib.Ash.PoliciesTest do
     end
 
     assert {:error, %{}} =
-             Article
+             Article1
              |> Ash.Changeset.for_create(:create)
              |> Ash.create()
+  end
+
+  test "resource policies preced fragment policies" do
+    defmodule Article2Fragment do
+      use Spark.Dsl.Fragment,
+        of: Ash.Resource,
+        authorizers: [Ash.Policy.Authorizer]
+
+      policies do
+        policy always() do
+          authorize_if never()
+        end
+      end
+    end
+
+    defmodule Article2 do
+      use Ash.Resource,
+        domain: SonetLib.Domain,
+        authorizers: [Ash.Policy.Authorizer],
+        fragments: [
+          Article2Fragment
+        ]
+
+      attributes do
+        uuid_primary_key :id
+        attribute :title, :string
+      end
+
+      actions do
+        defaults [:read, :destroy, create: :*, update: :*]
+      end
+
+      policies do
+        bypass always() do
+          authorize_if always()
+        end
+      end
+    end
+
+    assert Article2
+           |> Ash.Changeset.for_create(:create)
+           |> Ash.create!()
   end
 end
