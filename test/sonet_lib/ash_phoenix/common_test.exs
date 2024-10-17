7da71b091178f5f77271c198df2e9b2351f53988
Minsub Kim <rapidfsub@gmail.com>
2024-10-17 20:03:36 +0900

    fix test

--- test/sonet_lib/ash_phoenix/common_test.exs
+++ test/sonet_lib/ash_phoenix/common_test.exs
@@ -3,20 +3,55 @@ defmodule SonetLib.AshPhoenix.CommonTest do
 
   test "create" do
     assert TestIdentity.User
-           |> AshPhoenix.Form.for_create(:create)
-           |> AshPhoenix.Form.submit!()
+           |> Form.for_create(:create,
+             params: %{
+               "email" => Faker.Internet.email()
+             }
+           )
+           |> Form.submit!()
   end
 
   test "create_with_stores" do
     assert %{stores: stores} =
              TestIdentity.User
-             |> AshPhoenix.Form.for_create(:create_with_stores,
+             |> Form.for_create(:create_with_stores,
                params: %{
-                 stores: [%{handle: "store1"}, %{handle: "store2"}]
+                 "email" => Faker.Internet.email(),
+                 "stores" => [%{"handle" => "store1"}, %{"handle" => "store2"}]
                }
              )
-             |> AshPhoenix.Form.submit!()
+             |> Form.submit!()
 
     assert_value Enum.map(stores, &to_string(&1.handle)) == ["store1", "store2"]
+  end
+
+  describe "with user" do
+    setup do
+      user = Ashex.seed!(TestIdentity.User, %{email: Faker.Internet.email()})
+
+      stores =
+        for i <- 1..10 do
+          %Shopify.Store{handle: "store_#{i}", user_id: user.id}
+        end
+        |> Ashex.seed!()
+
+      %{user: user, stores: stores}
+    end
+
+    test "update", %{user: user} do
+      old_email = user.email
+      assert user.email == old_email
+
+      assert user =
+               user
+               |> Form.for_update(:update,
+                 params: %{
+                   "email" => Faker.Internet.email()
+                 }
+               )
+               |> Form.submit!()
+
+      refute user.email == old_email
+    end
   end
 end
