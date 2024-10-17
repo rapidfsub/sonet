a343007b480fc5fb72258e4f29ea7a782b59126b
Minsub Kim <rapidfsub@gmail.com>
2024-10-17 19:30:54 +0900

    fix test

--- test/sonet_lib/ash_phoenix/common_test.exs
+++ test/sonet_lib/ash_phoenix/common_test.exs
@@ -8,13 +8,15 @@ defmodule SonetLib.AshPhoenix.CommonTest do
   end
 
   test "create_with_stores" do
-    assert %{stores: [%{}, %{}]} =
+    assert %{stores: stores} =
              SonetLib.TestIdentity.User
              |> AshPhoenix.Form.for_create(:create_with_stores,
                params: %{
-                 stores: [%{}, %{}]
+                 stores: [%{handle: "store1"}, %{handle: "store2"}]
                }
              )
              |> AshPhoenix.Form.submit!()
+
+    assert_value Enum.map(stores, &to_string(&1.handle)) == ["store1", "store2"]
   end
 end
