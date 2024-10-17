15cb7f8857c6fb57f44e6ac10d6e1a3bd0c7fcae
Minsub Kim <rapidfsub@gmail.com>
2024-10-17 19:23:52 +0900

    test create_with_stores

--- test/sonet_lib/ash_phoenix/common_test.exs
+++ test/sonet_lib/ash_phoenix/common_test.exs
@@ -6,4 +6,15 @@ defmodule SonetLib.AshPhoenix.CommonTest do
            |> AshPhoenix.Form.for_create(:create)
            |> AshPhoenix.Form.submit!()
   end
+
+  test "create_with_stores" do
+    assert %{stores: [%{}, %{}]} =
+             SonetLib.TestIdentity.User
+             |> AshPhoenix.Form.for_create(:create_with_stores,
+               params: %{
+                 stores: [%{}, %{}]
+               }
+             )
+             |> AshPhoenix.Form.submit!()
+  end
 end
