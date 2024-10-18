f370305753cc30a138632e51e179f1d5b8aaf510
Minsub Kim <rapidfsub@gmail.com>
2024-10-18 16:30:48 +0900

    add Enumex.deep_map_key

--- lib/sonet_lib/enumex.ex
+++ lib/sonet_lib/enumex.ex
@@ -12,24 +12,59 @@ defmodule SonetLib.Enumex do
 
   """
   def deep_delete_key(%{} = map, key) do
-    Map.delete(map, key)
-    |> Map.new(fn {k, v} ->
+    for {k, v} <- Map.delete(map, key), into: %{} do
       {k, deep_delete_key(v, key)}
-    end)
+    end
   end
 
   def deep_delete_key([{k, _v} | _] = keywords, key) when is_atom(k) do
-    Keyword.delete(keywords, key)
-    |> Keyword.new(fn {k, v} ->
+    for {k, v} <- Keyword.delete(keywords, key) do
       {k, deep_delete_key(v, key)}
-    end)
+    end
   end
 
   def deep_delete_key(list, key) when is_list(list) do
-    Enum.map(list, &deep_delete_key(&1, key))
+    for value <- list do
+      deep_delete_key(value, key)
+    end
   end
 
   def deep_delete_key(value, _key) do
+    value
+  end
+
+  @doc ~S"""
+  ## Examples
+
+      iex> Enumex.deep_map_key(%{b: [a: 1, b: 2, a: 3], c: [a: 1, b: 2, a: 3]}, &to_string/1)
+      %{"b" => [a: 1, b: 2, a: 3], "c" => [a: 1, b: 2, a: 3]}
+
+      iex> Enumex.deep_map_key([a: 1, b: 2, c: %{a: 1, b: 2, c: 3}], &to_string/1)
+      [a: 1, b: 2, c: %{"a" => 1, "b" => 2, "c" => 3}]
+
+      iex> Enumex.deep_map_key([%{a: 1, b: 2}, %{a: 1, b: 2}], &to_string/1)
+      [%{"a" => 1, "b" => 2}, %{"a" => 1, "b" => 2}]
+
+  """
+  def deep_map_key(%{} = map, fun) do
+    for {k, v} <- map, into: %{} do
+      {fun.(k), deep_map_key(v, fun)}
+    end
+  end
+
+  def deep_map_key([{k, _v} | _] = keywords, fun) when is_atom(k) do
+    for {k, v} <- keywords do
+      {k, deep_map_key(v, fun)}
+    end
+  end
+
+  def deep_map_key(list, fun) when is_list(list) do
+    for value <- list do
+      deep_map_key(value, fun)
+    end
+  end
+
+  def deep_map_key(value, _fun) do
     value
   end
 end
