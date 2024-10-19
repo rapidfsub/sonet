08c0363d3e337acb3a9d484917d800887d1074dc
Minsub Kim <rapidfsub@gmail.com>
2024-10-19 17:50:25 +0900

    add User seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,23 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+use Sonet.Prelude
+
+user_data =
+  [
+    %{email: "arnaldo2002@example.org", username: "Timeless Vapes"},
+    %{email: "myrna.maggio@example.net", username: "Colorado Cannabis Company"},
+    %{email: "haylee1965@example.com", username: "CI Wholesale"},
+    %{email: "warren2094@example.com", username: "Jetty Extracts"},
+    %{email: "myrtice2094@example.org", username: "Bloom Farms"},
+    %{email: "ardith_schulist@example.org", username: "THC Factory"},
+    %{email: "antwon.hahn@example.org", username: "Oil Stix"},
+    %{email: "wilton2002@example.net", username: "Caviar Gold"},
+    %{email: "josue_prohaska@example.net", username: "Shore Natural RX"},
+    %{email: "lola.jakubowski@example.net", username: "Legal Drinks"}
+  ]
+
+for params <- user_data do
+  params = Map.merge(params, %{password: "password", password_confirmation: "password"})
+  Ashex.run_create!(Accounts.User, :register_with_password, params: params)
+end
