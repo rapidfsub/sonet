e02f64a15544729e8fc5d5dd10b1691108aae4a5
Minsub Kim <rapidfsub@gmail.com>
2024-10-21 09:06:32 +0900

    move common path to server url

--- lib/sonet_web/ash_json_api_router.ex
+++ lib/sonet_web/ash_json_api_router.ex
@@ -8,9 +8,21 @@ defmodule SonetWeb.AshJsonApiRouter do
     open_api: "/open_api",
     modify_open_api: {__MODULE__, :modify_open_api, []}
 
+  @prefix "/api/json"
   def modify_open_api(%OpenApiSpex.OpenApi{} = spec, _, _) do
     schemes = %{"authorization" => %OpenApiSpex.SecurityScheme{type: "http", scheme: "bearer"}}
     components = %{spec.components | securitySchemes: schemes}
-    %{spec | components: components, security: [%{"authorization" => []}]}
+    security = [%{"authorization" => []}]
+
+    [%OpenApiSpex.Server{} = server] = spec.servers
+    url = Path.join([server.url, @prefix])
+    servers = [%{server | url: url}]
+
+    paths =
+      Map.new(spec.paths, fn {k, v} ->
+        {String.replace_prefix(k, @prefix, ""), v}
+      end)
+
+    ~M{spec | components, security, servers, paths}
   end
 end
