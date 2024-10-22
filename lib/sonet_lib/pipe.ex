d12520b5fb3563323bc3fb5bd45746058d0072d1
Minsub Kim <rapidfsub@gmail.com>
2024-10-22 12:47:00 +0900

    add pipe for tuple with atom key

--- lib/sonet_lib/pipe.ex
+++ lib/sonet_lib/pipe.ex
@@ -1,4 +1,8 @@
 defmodule SonetLib.Pipe do
+  defmacro last_arg ~> atom when is_atom(atom) do
+    {atom, last_arg}
+  end
+
   defmacro last_arg ~> {name, meta, args} do
     {name, meta, List.insert_at(args, -1, last_arg)}
   end
